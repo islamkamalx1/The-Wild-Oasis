83ee12672365c6a2a629d3c1013e5499aab6f64b
Islam Kamal <islamkamalx1>
2024-01-08 23:52:39 +0200

    create tables in supabase, relations between them, and connect with the App

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://vtoioyhwtkbwxyzuvrca.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZ0b2lveWh3dGtid3h5enV2cmNhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDQ3Mzk3OTksImV4cCI6MjAyMDMxNTc5OX0.1Ilm8XRIKfizE-YYCeUTU-mINAQCMZj7ra4E6n_o24A";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
